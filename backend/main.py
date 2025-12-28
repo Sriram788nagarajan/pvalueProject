94cfb7f3edc7fca44749516f0e66b1fea0c8f833
N SRIRAM <arizn788@gmail.com>
2025-12-28 10:40:36 +0530

    Allow pvalue.net origin in CORS

--- backend/main.py
+++ backend/main.py
@@ -34,7 +34,8 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
-        "https://sriram788nagarajan.github.io"
+        "https://sriram788nagarajan.github.io",
+        "https://pvalue.net"
     ],
     allow_credentials=True,
     allow_methods=["*"],
